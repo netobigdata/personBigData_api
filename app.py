77b7596b0d855b573e62e2a696682f26ceddb0b2
netobigdata <eromanbigdata@gmail.com>
2024-03-07 21:02:12 -0600

    Adding information requiremnts and new Docker file structure

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ client = bigquery.Client()
 @app.route('/persona', methods=['POST'])
 def save_persona():
     data = request.json
-    table_id = "your-project-id.your-dataset.persona"
+    #table_id = "your-project-id.your-dataset.persona"
+    table_id = "myprojectbigdatadev.dtset_level_stage.tbl_persona"
 
     errors = client.insert_rows_json(table_id, [data])  # Make sure your data keys match the BigQuery table schema
 
