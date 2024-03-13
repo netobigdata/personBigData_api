1463c26125470fe4452383141f7fcbd678e2b77c
netobigdata <eromanbigdata@gmail.com>
2024-03-12 19:22:51 -0600

    Adding Github Actions Configuration

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,14 @@
-# Use an official Python runtime as a parent image
-FROM python:3.8-slim
- 
-# Set the working directory in the container
+FROM python:3.9-slim
+
 WORKDIR /app
- 
-# Copy the current directory contents into the container at /app
+
 COPY . /app
- 
-# Install any needed packages specified in requirements.txt
-RUN pip install --no-cache-dir -r requeriminetos.txt
- 
-# Make port 8080 available to the world outside this container
+COPY requirements.txt /app
+
+RUN pip install --no-cache-dir -r requirements.txt
+
 EXPOSE 8080
- 
-# Define environment variable
+
 ENV PORT 8080
- 
-# Run app.py when the container launches
+
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -1,20 +1,32 @@
 from flask import Flask, request, jsonify
 from google.cloud import bigquery
+import os
 
 app = Flask(__name__)
 client = bigquery.Client()
 
-@app.route('/persona', methods=['POST'])
-def save_persona():
-    data = request.json
-    #table_id = "your-project-id.your-dataset.persona"
-    table_id = "myprojectbigdatadev.dtset_level_stage.tbl_persona"
+# Replace with your BigQuery dataset and table name
+dataset_name = 'ds_company'
+table_name = 'tbl_persona'
 
-    errors = client.insert_rows_json(table_id, [data])  # Make sure your data keys match the BigQuery table schema
+@app.route('/person', methods=['POST'])
+def add_person():
+    content = request.json
+    doc_identidad = content['doc_identidad']
+    nombre = content['nombre']
+    apellido = content['apellido']
 
+    table_id = f"{client.project}.{dataset_name}.{table_name}"
+
+    rows_to_insert = [
+        {u"doc_identidad": doc_identidad, u"nombre": nombre, u"apellido": apellido}
+    ]
+
+    errors = client.insert_rows_json(table_id, rows_to_insert)  # Make an API request.
     if errors == []:
         return jsonify({"success": True}), 200
-    return jsonify({"success": False, "errors": errors}), 500
+    else:
+        return jsonify({"success": False, "errors": errors}), 500
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True)
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
